b692de981e453b7da81d4a69ab29219b1714c6df
Genilson Souza <genilson.souza@sesims.com.br>
2024-03-13 09:57:22 -0400

    Create Section Header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,9 +5,11 @@ import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Signin from './pages/Signin';
 import Dashboard from './pages/Dashboard'; 
+import Header from './components/Header';
 export default function App(){
   return(
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
